8b973e7affa3272414ff1870c4d94e096db31629
turisap <kirillshakirov57@gmail.com>
2021-01-20 00:11:42 +0300

    Add writing a new CSS asset

--- src/plugin/plugin.js
+++ src/plugin/plugin.js
@@ -1,16 +1,16 @@
 const { validate } = require("schema-utils");
 const schema = require("./schema.json");
+const { RawSource } = require("webpack-sources");
 
 // TODO: check if it works with css-modules
 // TODO: add comment to each media query about which src file it comes from
 
+//FIXME: fix BREAKING CHANGE WARNING FROM WEBPACK
+
 class ExtractMediaQueriesPlugin {
     static pluginName = "ExtractMediaQueriesPlugin";
     static optionsList = ["oneFile"];
-    static mediaQueryRegex = new RegExp(
-        "@media[^{]+{([^{}]*{[^}{]*})+[^}]+}",
-        "g",
-    );
+    static mediaQueryRegex = new RegExp("@media[^{]+{([^{}]*{[^}{]*})+[^}]+}", "g");
 
     #options;
 
@@ -25,23 +25,32 @@ class ExtractMediaQueriesPlugin {
         });
     }
 
-    _extractQueries(source) {
-        const queries = source.match(ExtractMediaQueriesPlugin.mediaQueryRegex);
+    _createAsset(queries, compilation) {
+        // TODO: make 'em dynamic
+        compilation.assets["ololo.css"] = new RawSource(queries);
+    }
 
-        console.log(queries);
+    _removeQueriesFromSource() {}
+
+    _extractQueries(source) {
+        const queries = source
+            .match(ExtractMediaQueriesPlugin.mediaQueryRegex)
+            .join("\n \n");
+
+        return queries;
     }
 
     _process(compilation) {
-        const assetStyleNames = Object.keys(compilation.assets).filter(
-            (assetName) => {
-                return /^\w+.css$/.test(assetName);
-            },
-        );
+        const assetStyleNames = Object.keys(compilation.assets).filter((assetName) => {
+            return /^\w+.css$/.test(assetName);
+        });
 
         if (!Boolean(assetStyleNames.length)) return Promise.resolve(null);
 
         assetStyleNames.forEach((styleAsset) => {
-            this._extractQueries(compilation.assets[styleAsset].source());
+            const queries = this._extractQueries(compilation.assets[styleAsset].source());
+
+            this._createAsset(queries, compilation);
         });
 
         return Promise.resolve();
@@ -52,8 +61,6 @@ class ExtractMediaQueriesPlugin {
             ExtractMediaQueriesPlugin.pluginName,
             (compilation) =>
                 this._process(compilation).then((result) => {
-                    // this.deleteAssets(compilation);
-                    // return result;
                     return compilation;
                 }),
         );
