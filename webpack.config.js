f6f73f59451d515f4bf92dae617a0c8591ac072d
turisap <kirillshakirov57@gmail.com>
2021-02-02 23:55:38 +0300

    Add main script

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { ExtractMediaQueriesPlugin } = require("./src/plugin");
+const { ExtractMediaQueriesPlugin } = require("extract-media-query-plugin");
 
 module.exports = {
     entry: "./src/index.tsx",
